e0db03e8738e0d29ccf169b58ed25d7a399a3819
Leslye Adanaque <adanaquemazaleslyegianella25@gmail.com>
2025-04-23 23:35:48 -0500

    Arreglo en personality

--- src/personality/entities/personality.entity.ts
+++ src/personality/entities/personality.entity.ts
@@ -1,1 +1,15 @@
-export class Personality {}
+// personality.entity.ts
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Personality {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  // Este campo es el que se usa para el "soft delete"
+  @Column({ default: false })
+  deleted: boolean;
+}
