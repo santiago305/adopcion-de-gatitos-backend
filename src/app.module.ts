789f4cd9d9b8d9160712c07948447f50f815725a
santiago305 <minecratf633@gmail.com>
2025-05-06 00:57:55 -0500

    feat: implement authentication and user management modules with controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { RolesModule } from './roles/roles.module';
+import { UsersModule } from './users/users.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule, 
+    RolesModule, 
+    UsersModule
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(TokenMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
